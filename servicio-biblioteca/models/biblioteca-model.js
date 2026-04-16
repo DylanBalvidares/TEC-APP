932b2b730ab0a177eb85020eb8a00cd0bea34f4c
Dylan Balvidares <Dylanaza037@gmail.com>
2026-04-15 22:51:40 -0300

    biblioteca models(no relations)

---
+++ servicio-biblioteca/models/biblioteca-model.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db/conexionDB.js";
+
+const Biblioteca = sequelize.define(
+  "biblioteca",
+  {
+    id_biblioteca: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    ubicacion: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    responsable: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "biblioteca",
+    timestamps: false,
+  },
+);
+
+export default Biblioteca;
